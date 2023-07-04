acdf8d924fd18aa8a421226970d2398c40dc8780
alexisadrian.carrillo <alexisadrian.carrillo@bbva.com>
2023-07-03 20:08:14 -0600

    Added: App, Constants, Fastapi, /data Endpoint

---
+++ main.py
@@ -0,0 +1,80 @@
+#------------- Imports -----------------
+from numpy.random import (
+    randint,
+    uniform,
+    choice
+)
+from src.constants import(
+    COL_LIST,
+    REGISTRES,
+    BRONZEPATH,
+    DATELOW,
+    DATEHIGH,
+    GOODCOMMENTS,
+    MEDCOMMENTS,
+    BADCOMMENTS
+)
+from pandas import (
+    DataFrame,
+    read_csv
+)
+from src.date.random_date import(
+    random_date
+)
+from fastapi import FastAPI
+import uvicorn
+
+#------------- Config -----------------
+
+movies =  read_csv(BRONZEPATH + 'movies.csv')
+app = FastAPI()
+
+#----------- Main Code ----------------
+
+@app.get(
+        '/',
+        status_code=200
+)
+async def root():
+
+    """
+    Endpoint for health check
+    """
+
+    return {"Health": "Ok"}
+
+@app.post(
+        "/data",
+        status_code=200
+)
+async def generate_random_samples():
+
+    """
+    Endpoint for generating movie data
+    """
+
+    moviesRandom = movies.sample(n = REGISTRES, replace = True)
+    moviesRandom = moviesRandom[['title', 'genres']]
+
+    users = randint(10, 50, size=REGISTRES)
+    ages = randint(18, 60, size=REGISTRES)
+    rating = [round(x,1) for x in uniform(1, 5, size=REGISTRES)]
+    dates = [random_date(DATELOW, DATEHIGH) for i in range(REGISTRES)]
+    comments = [ choice(GOODCOMMENTS) if x > 3.9 else choice(MEDCOMMENTS) if x > 2.9 else choice(BADCOMMENTS) for x in rating ]
+
+    moviesFinal = DataFrame(
+        {
+            COL_LIST[0]: users,
+            COL_LIST[1]: ages,
+            COL_LIST[2]: moviesRandom['title'].tolist(),
+            COL_LIST[3]: moviesRandom['genres'].tolist(),
+            COL_LIST[4]: rating,
+            COL_LIST[5]: comments,
+            COL_LIST[6]: dates
+        }
+    )
+
+    return moviesFinal.to_json(orient = 'records')
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="localhost", port=5000)
---
+++ src/constants.py
@@ -0,0 +1,117 @@
+from numpy.random import (
+    randint
+)
+import configparser
+
+config = configparser.ConfigParser()
+config.read(r'config.ini')
+
+
+BRONZEPATH=config['DATAPATH']['BRONZEPATH']
+
+COL_LIST=[
+    'userid',
+    'age',
+    'title',
+    'genres',
+    'rating',
+    'commentary',
+    'date'
+]
+
+REGISTRES=randint(3000, 9000, size=1)[0]
+
+DATELOW = "1/1/2015 1:30 PM"
+DATEHIGH = "1/1/2023 4:50 AM"
+
+GOODCOMMENTS = [
+    'Genial',
+    'Buena pelicula, interesente y captiva',
+    'Pelicula divertida',
+    'Obra de arte',
+    'Pelicual de Oscar!!!',
+    'No puedo dejar de verla',
+    'La he repetido mas de 3 veces',
+    'Entretenida',
+    'Quiero verla una y otra vez',
+    'Me gusto',
+    'Sin Comentario',
+    'Pocas peliculas como esta',
+    'Sin dudar de las mejores',
+    'Hay peliculas malas y luego existen estas bellezas',
+    'Quisiera volver a verla',
+    'Me gusto tanto q la vi varias veces',
+    'q joya acabo de ver',
+    'No puedo creer q existan este tipio de peliculas!',
+    'Buena',
+    'Interesenta',
+    'Captiva',
+    'Entretenida',
+    'Mucha accion!',
+    'Buenisima!!!!!!',
+    'Super buena!!!!!!!',
+    'Buena!',
+    'Entretenida!',
+    'Me encanto!!',
+    'Top 10',
+    'Buena buena buena',
+    'Excelente!',
+    'Una pelicula increible',
+    'Un clasico',
+    'Inolvidable',
+    'Brillante, Brillante, Brillante',
+    'Simple pero buena'
+]
+
+MEDCOMMENTS = [
+    'Ni buena, ni mala',
+    'Meh',
+    'He visto mejores',
+    'Mas o menos',
+    'Sin Comentario',
+    'Poco entretenida',
+    'q pelicula mas x',
+    'Sin duda, nada nuevo',
+    'He bisto peores',
+    'No me encato',
+    'Media',
+    'Promedio',
+    'Lo mismo de siempre',
+    'Mah',
+    'Nada nuevo',
+    'Una vez y ya',
+    'He visto peores'
+]
+
+BADCOMMENTS = [
+    'Pesima!',
+    'Me dormi viendola',
+    'Me arrepiento de verla',
+    'Una pelicula que paso sin pena ni gracia',
+    'Sin Comentario',
+    'Muy aburridaaaaa!!',
+    'Muy aburrida',
+    'He visto peores',
+    'Pfff, prefiero no hablar de ella',
+    'No vale la pena verla!',
+    'No vale la pena verla!',
+    'Desperdicio de dinero',
+    'Hay peliculas malas y luego esta esta',
+    'No se que poner',
+    'Prefiero no decir nada...',
+    'No la vean',
+    'Le hacen un favor al mundo al no ver tremenda caga...',
+    'quieranse y vean otra cosa',
+    'Mala',
+    'Horrible',
+    'Pesima!!!!!!!',
+    'Pesima',
+    'Malisima!!!',
+    'Mala mala mala',
+    'Malisima',
+    'Horrible!',
+    'Absurdamente mala',
+    'Si pudiera darle menos, lo haria',
+    'No para ser tomada enserio',
+    'Intenta hacer una peor pelicula...'
+]
